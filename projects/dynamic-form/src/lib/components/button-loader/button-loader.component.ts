658eef082137403ff92262c955cc1d8af9c88c7a
Davide Rubiu <daviderubiu5@gmail.com>
2024-05-18 12:36:41 +0200

    Adding file to lib

---
+++ projects/dynamic-form/src/lib/components/button-loader/button-loader.component.ts
@@ -0,0 +1,59 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-button-loader',
+  templateUrl: './button-loader.component.html',
+  styleUrls: ['./button-loader.component.scss']
+})
+export class ButtonLoaderComponent implements OnInit {
+  @Input() loadSpinner: boolean = false
+  @Input() loadCheck: boolean = false
+  @Input() isFormValid: boolean = true
+  @Input() text: string = ''
+  @Input() icon: string = ''
+  @Input() type: string = 'submit'
+  @Input() color: string = 'primary'
+  @Input() text_color: string = 'white'
+
+  constructor() { }
+  ngOnInit() {
+  }
+  getClassFromIcon():string{
+    if(this.icon=='credit-card'){
+      return 'uil uil-credit-card'
+    }
+    if(this.icon=='deliveroo'){
+      return 'uil uil-credit-card'
+    }
+    if(this.icon=='back'){
+      return 'uil uil-angle-left-b'
+    }
+    if(this.icon=='info'){
+      return 'uil uil-info-circle'
+    }
+    return this.icon
+  }
+  getTextColor(){
+    if(this.text_color=='white'){
+      return '#fff'
+    }
+    if(this.text_color=='deliveroo'){
+      return '#35b8b2'
+    }
+    if(this.text_color=='red'){
+      return '#e2626b'
+    }
+    if(this.text_color=='green'){
+      return '#45c4a0'
+    }
+    if(this.text_color=='yellow'){
+      return '#fab758'
+    }
+    if(this.text_color=='pink'){
+      return '#d16b86'
+    }
+    return this.text_color
+  }
+
+
+}
--- projects/dynamic-form/src/lib/dynamic-form.module.ts
+++ projects/dynamic-form/src/lib/dynamic-form.module.ts
@@ -4,10 +4,12 @@ import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { DynamicFormComponent } from "./dynamic-form.component";
 import { NgxSliderModule } from '@angular-slider/ngx-slider';
 import { NgbCalendar, NgbCalendarGregorian, NgbModule, NgbTimepicker } from "@ng-bootstrap/ng-bootstrap";
+import { ButtonLoaderComponent } from "./components/button-loader/button-loader.component";
 
 @NgModule({
   declarations: [
-    DynamicFormComponent
+    DynamicFormComponent,
+    ButtonLoaderComponent
   ],
   imports : [
     CommonModule,
