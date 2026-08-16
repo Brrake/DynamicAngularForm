634d074d626636520bae48456444f5d848b05211
Davide Rubiu <rubiu@dauvea.it>
2026-08-16 15:09:55 +0200

    Aggiorna la documentazione del README per riflettere le nuove versioni di Node.js e Angular, e migliora la sezione delle dipendenze peer.

--- Tools/publish.py
+++ Tools/publish.py
@@ -34,13 +34,16 @@ if __name__ == '__main__':
     npm_otp = input("Insert OTP Code: ").strip().lower()
     subprocess.call([npm_cmd, 'publish','--otp', npm_otp ,'--tag',release], cwd=dist_dir)
     print('✅ Publish completed')
-        
+    with open(project_dir / 'package.json', 'r') as f:
+        package_json = json.load(f)
+        version = package_json['version']
+        print(f"Version: {version}")
+    latest_npm = input(f"Do you want to publish {version} as latest NPM? (Y/N): ").strip().lower()
+    if latest_npm in ['y', 'yes']:
+        subprocess.call([npm_cmd, 'dist-tag','add',f'{lib_name}@{version}','latest'], cwd=main_dir)
+        print('✅ Latest NPM published')
     github_tags = input("Do you want to publish GitHub Tags? (Y/N): ").strip().lower()
     if github_tags in ['y', 'yes']:
-        with open(project_dir / 'package.json', 'r') as f:
-            package_json = json.load(f)
-            version = package_json['version']
-            print(f"Version: {version}")
         subprocess.call(['git', 'tag','v'+version], cwd=main_dir)
         subprocess.call(['git', 'push','origin','v'+version], cwd=main_dir)
         print('✅ GitHub Tags published')
